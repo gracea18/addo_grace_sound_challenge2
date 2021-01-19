41f26d077c3aa57cd7dfa2de424c7f2ca5d8fd83
gracea18 <72771341+gracea18@users.noreply.github.com>
2021-01-19 00:15:58 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
     print("Sound Level" + input.sound_level())
-  
-    if input.sound_level() > 20:
-        music.siren.play()
-
+    
+    if input.sound_level() < 20:
+        music.set_volume(100)
+        music.siren.play_until_done()
 
 
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 while (true) {
     console.log("Sound Level" + input.soundLevel())
-    if (input.soundLevel() > 20) {
-        music.siren.play()
+    if (input.soundLevel() < 20) {
+        music.setVolume(100)
+        music.siren.playUntilDone()
     }
     
 }
